caa14dbee8d5d9b0e8f66cf285d7de83b1cdc68a
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2021-12-03 06:05:11 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -916,7 +916,7 @@ module.exports = handle = (client, Client) => {
  	                 const mediaMsg = await client.prepareMessageMedia(await getBuffer(configs.imgUrl), 'imageMessage')
                      const buttonMessage = {
                            contentText: menu(data.prefix, data.pushname),
-                           footerText: 'HANZ々OFC彡',
+                           footerText: 'TIO-OFFICIAL彡',
                                 "contextInfo": {
 									  mentionedJid: [configs.ownerList[0]],
                                       participant: sender,
